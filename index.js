e3d46e1d54b16fe87e73d46795d8ecb4933cba1b
Subodh <subodh186@gmail.com>
2014-11-14 02:47:34 +0000

    Added index.html input

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express();
+var fs=require('fs');
+
+var mybuff= new Buffer(fs.readFileSync('index.html'));
+var mystr = mybuff.toString();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(mystr)
 })
 
 app.listen(app.get('port'), function() {
