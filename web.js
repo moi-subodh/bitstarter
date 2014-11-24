5582328cf08596f93517cca4b66ed42f7b47d5d9
Subodh <subodh186@gmail.com>
2014-11-24 03:49:14 +0000

    added web.js to git

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express();
+var fs=require('fs');
+
+var mybuff= new Buffer(fs.readFileSync('index.html'));
+var mystr = mybuff.toString();
+
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send(mystr)
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
